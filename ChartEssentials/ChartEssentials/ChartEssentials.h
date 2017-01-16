3b2067653c69ea56f845141f0d313ba979ac70ea
Anton Vaneev <avaneyev@gmail.com>
2017-01-16 13:47:58 -0500

    Initial project created, basic targets added.

---
+++ ChartEssentials/ChartEssentials/ChartEssentials.h
@@ -0,0 +1,27 @@
+//
+//  ChartEssentials.h
+//  ChartEssentials
+//
+//  Created by Anton Vaneev.
+//  Copyright (c) 2017-present, Anton Vaneev. All rights reserved.
+//
+//  Distributed under BSD license. See LICENSE for details.
+//
+
+#if TARGET_OS_IPHONE
+
+#import <UIKit/UIKit.h>
+
+#elif TARGET_OS_MAC
+
+#endif
+
+//! Project version number for ChartEssentials.
+FOUNDATION_EXPORT double ChartEssentialsVersionNumber;
+
+//! Project version string for ChartEssentials.
+FOUNDATION_EXPORT const unsigned char ChartEssentialsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ChartEssentials/PublicHeader.h>
+
+
