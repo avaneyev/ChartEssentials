3e4959031af2746946ae9660088a01b4eadefec4
Anton Vaneev <avaneyev@gmail.com>
2017-01-21 22:46:15 -0500

    Test adding more values one by one.

--- ChartEssentials/ChartEssentialsTests/DataTable/CEValueColumnTests.m
+++ ChartEssentials/ChartEssentialsTests/DataTable/CEValueColumnTests.m
@@ -35,4 +35,24 @@
     XCTAssertEqual([column valueAtIndex:0], addedValue);
 }
 
+- (void)testAddingMultipleValuesOneByOne
+{
+    CEValueColumn *column = [[CEValueColumn alloc] init];
+
+    for (NSUInteger i = 0; i < 1005; i++)
+    {
+        CGFloat value = i + 10;
+        [column addValue:value];
+    }
+    
+    XCTAssertEqual(column.count, 1005);
+    XCTAssertEqualWithAccuracy(column.first, 10, 0.001);
+    XCTAssertEqualWithAccuracy(column.last, 1014, 0.001);
+    
+    XCTAssertEqualWithAccuracy([column valueAtIndex:70], 80, 0.001);
+    XCTAssertEqualWithAccuracy([column valueAtIndex:135], 145, 0.001);
+    XCTAssertEqualWithAccuracy([column valueAtIndex:955], 965, 0.001);
+    XCTAssertEqualWithAccuracy([column valueAtIndex:999], 1009, 0.001);
+}
+
 @end
