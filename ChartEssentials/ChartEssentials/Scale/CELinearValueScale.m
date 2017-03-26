4f0811e3245782ca9fe5e53b207baa95de0563a7
Anton Vaneev <avaneyev@gmail.com>
2017-03-25 23:30:50 -0400

    WIP determining scale.

--- ChartEssentials/ChartEssentials/Scale/CELinearValueScale.m
+++ ChartEssentials/ChartEssentials/Scale/CELinearValueScale.m
@@ -11,6 +11,76 @@
 #import <ChartEssentials/CELinearValueScale.h>
 #import <ChartEssentials/CEScaleHint.h>
 #import <ChartEssentials/CETools.h>
+#import <ChartEssentials/CEPlatformDefinitions.h>
+
+// Function determines 'magnitude' of a value for the purpose of rounding markers.
+// The goal is to pick the magnitude so that the result of dividing the value by it is between 10 and 100.
+// Power is the corresponding power of 10 to get the magnitude.
+// For example:
+// - magnitude of 12345 is 1000, and 12345 is considered on the order of thousands between 12000 and 13000;
+// - magnitude of 10 is 1, 10 is considered on the order of singles and will be either between 9 and 11;
+// - magnitude of 0.25 is 0.01, the value is in one-hundredths and is between 0.24 and 0.26.
+//
+// Then magnitude could be used for rounding that appears meaningful, because other values being scaled
+// are likely to be of the same magnitude as values usually fluctuate around.
+// When the values don't fluctuate around some value, another type of scale (for example, logarithmic)
+// may be more appropriate.
+static void _CELinearValueScaleGetMagnitude(CGFloat value, NSInteger *outPower, CGFloat *outMagnitude)
+{
+    CEAssert(value >= 0);
+    
+    if (value == 0)
+    {
+        *outPower = 0;
+        *outMagnitude = 0;
+        return;
+    }
+    
+    if (isnan(value))
+    {
+        *outPower = NAN;
+        *outMagnitude = NAN;
+        return;
+    }
+    
+    CGFloat currentValue = value;
+    *outPower = 0;
+    *outMagnitude = 1;
+    
+    while (currentValue > 100)
+    {
+        ++(*outPower);
+        *outMagnitude *= 10;
+        currentValue /= 10;
+    }
+    
+    if (*outPower == 0)
+    {
+        while (currentValue < 10)
+        {
+            --(*outPower);
+            *outMagnitude /= 10;
+            currentValue *= 10;
+        }
+    }
+}
+
+static NSArray<CEAxisMarker *> *_CELinearValueScaleEqualMarkers(CGFloat low, CGFloat high, NSUInteger count)
+{
+    CEAssert(count > 0);
+    
+    CGFloat step = (high - low) / count;
+    NSMutableArray<CEAxisMarker *> *markers = [[NSMutableArray alloc] initWithCapacity:count];
+    [markers addObject:[[CEAxisMarker alloc] initWithCaption:@"" value:low]];
+    
+    for (NSUInteger i = 1; i < count; ++i)
+    {
+        low += step;
+        [markers addObject:[[CEAxisMarker alloc] initWithCaption:@"" value:low]];
+    }
+    
+    return markers;
+}
 
 @implementation CELinearValueScale
 {
@@ -42,6 +112,7 @@
     [super setRenderHeight:renderHeight];
     
     // TODO: maybe don't recalculate for small changes?
+    _markers = nil;
 }
 
 - (CGFloat)scaleValue:(CGFloat)value
@@ -84,6 +155,33 @@
             if (hintValue < low) low = hintValue;
             if (hintValue > high) high = hintValue;
         }
+    }
+    
+    if (high == low)
+    {
+        // If the only known value is 0, there's nothing to be said about the value magnitude.
+        // Just pick an arbitrary range of [0, 1] and present the value on it.
+        if (low == 0)
+        {
+            _scaleLow = 0;
+            _scaleHigh = 1;
+            _markers = _CELinearValueScaleEqualMarkers(_scaleLow, _scaleHigh, 2);
+        }
+        else
+        {
+            // TODO: create 2 markers, one above and one below, based on scale of the values
+            // If the value happens to be rounded to scale, include it too
+        }
+    }
+    else
+    {
+        CGFloat magnitude;
+        NSInteger power;
+        
+        // Low value may be negative and have bigger absolute value that high value,
+        // need to get magnitude based on the value that has larger magnitude.
+        CGFloat valueForPower = MAX(cefabs(low), cefabs(high));
+        _CELinearValueScaleGetMagnitude(valueForPower, &power, &magnitude);
     }
 }
 
