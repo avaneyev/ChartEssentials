8aa0b9ec2c278b70c95047d947536b4dc35c6df0
Anton Vaneev <avaneyev@gmail.com>
2017-04-09 14:11:03 -0400

    Added a couple of properties to the chart view

--- ChartEssentials/ChartEssentials/View/CEChartView.h
+++ ChartEssentials/ChartEssentials/View/CEChartView.h
@@ -10,10 +10,32 @@
 
 #import <ChartEssentials/CEPlatformDefinitions.h>
 
+@class CEChartView;
+@class CEChartDefinition;
+
+/**
+ Chart view delegate can be used to get certain events about the chart activity, 
+ such as need for more data or presenting a menu.
+ */
+@protocol CEChartViewDelegate <NSObject>
+
+@end
+
+
 #if TARGET_OS_IPHONE
 @interface CEChartView : UIView
 #elif TARGET_OS_MAC
 @interface CEChartView : NSView
+#else
+#error Unsupported platform
 #endif
+
+/**
+ Definition describes the chart that should be rendered.
+ Updates to the definition object will cause chart to re-draw.
+ */
+@property (nonatomic, strong) CEChartDefinition *definition;
+
+@property (nonatomic, weak) id<CEChartViewDelegate> delegate;
 
 @end
--- ChartEssentials/ChartEssentials/View/CEChartView.m
+++ ChartEssentials/ChartEssentials/View/CEChartView.m
@@ -10,6 +10,25 @@
 
 #import <ChartEssentials/CEChartView.h>
 
+
 @implementation CEChartView
+{
+    CEChartDefinition *_definition;
+    
+    __weak id<CEChartViewDelegate> _delegate;
+}
+
+@synthesize delegate = _delegate;
+
+@synthesize definition = _definition;
+
+- (void)setDefinition:(CEChartDefinition *)definition
+{
+    if (_definition != definition)
+    {
+        _definition = definition;
+        // TODO: update subscriptions, redraw the chart.
+    }
+}
 
 @end
